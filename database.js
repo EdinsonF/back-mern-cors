090ca37e2621b226058cfc607517ecf511317a7d
edinson_figueroa <figueroaedinson@gmail.com>
2021-07-17 21:32:02 -0500

    iniciando

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB = 'mongodb://localhost/veterinaria';
+const DB = 'mongodb+srv://mongodb:13579@cluster0.w5soy.mongodb.net/veterinaria?retryWrites=true&w=majority';
 
 mongoose.connect(DB)
     .then(db => console.log('Base de Datos Conectada'))
